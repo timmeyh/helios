cd4233ceb0bb05910e40a89694eb4ed6351f32ec
Tim Vangehugten <timvangehugten@gmail.com>
2014-11-18 22:01:07 +0100

    Update t.py

--- tests/t.py
+++ tests/t.py
@@ -1,5 +1,5 @@
 import os
 import glob
 import testding
-modules = glob.glob(os.path.dirname(__file__)+"/*.py")
+modules = glob.glob(os.path.dirname(__file__)+"/coremods/*.py")
 __all__ = [ os.path.basename(f)[:-3] for f in modules]
